fe6a72c0bf4546a3007b96542320866eb97b2210
c4hyo <mdcnugroho@gmail.com>
2024-06-14 09:18:25 +0700

    add logo assets link

---
+++ lib/assets/logo.dart
@@ -0,0 +1,6 @@
+class YoAssets {
+  static String LOGO_BLACK =
+      "https://raw.githubusercontent.com/cahyo40/yo-assets/main/yo_black.png";
+  static String LOGO_WHITE =
+      "https://raw.githubusercontent.com/cahyo40/yo-assets/main/yo_white.png";
+}
